5e3cefa324794e27beaa311eef9c4de3e8a195d4
Alekei Grachev <aleksei.grachev@alberblanc.com>
2023-04-26 15:46:54 +0200

    added password

--- scripts/README.md
+++ scripts/README.md
@@ -2,6 +2,6 @@
 ```
 apt update && \
 apt install curl && \
-curl https://raw.githubusercontent.com/aljeshishe/sls-ssh-docker/master/scripts/install.sh | sh && \
+curl https://raw.githubusercontent.com/aljeshishe/sls-ssh-docker/master/scripts/install.sh | sh -s --password <password> && \
 curl https://raw.githubusercontent.com/aljeshishe/sls-ssh-docker/master/scripts/run.sh | sh
 ```
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
-set -ex
+
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+# Initialize our own variables:
+password=""
+verbose=0
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -p|--password) password="$2"; shift ;;
+        -v|--verbose) verbose=1 ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+echo "verbose=$verbose, password='$password', Leftovers: $@"
+if [[ -z "$password" ]];
+then
+    echo "-p or --password option required" 1>&2
+    exit 1
+fi
+
+if [[ $verbose == 1 ]];
+then
+    set -ex
+else
+    set -e
+fi
+
+
 
 mkdir -p /app 
 cd /app
@@ -8,7 +37,7 @@ apt update && DEBIAN_FRONTEND=noninteractive apt install sudo procps wget openss
 wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -O cloudflared
 chmod +x cloudflared
 mkdir -p /var/run/sshd
-echo root:root | chpasswd
+echo root:$password | chpasswd
 
 ssh-keygen -f /app/ssh_host_rsa_key -N '' -t rsa
 
